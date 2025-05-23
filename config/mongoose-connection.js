c15721292a8a9dfdb78a7c1d1a73efa3b3409502
Arpit Mahajan <130462117+Arpitmahajan123@users.noreply.github.com>
2025-05-23 06:25:18 +0530

    Updated

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const debgr = require('debug')("dev:mongoose");
 
 mongoose.connect('mongodb://127.0.0.1:27017/Bags')
 .then(function(){
-    console.log('Connected to MongoDB');
+    debgr('Connected to MongoDB');
 })
 .catch(function(err){
     console.log(err);
