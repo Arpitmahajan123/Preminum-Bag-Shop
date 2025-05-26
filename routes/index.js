bfc188072abbd49814bafbdcd12e1727521c6dc2
Arpit Mahajan <130462117+Arpitmahajan123@users.noreply.github.com>
2025-05-26 11:57:21 +0530

    Shop EJS

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middlewares/isLoggedIn');
+const flash = require('flash')
+
+// Assuming you have a User model defined in models/user.models.js
+
 
 // Importing the User model.
+
 router.get('/', (req, res) => {
-    res.render("index")
+    let error = req.flash("error");
+    res.render("index.ejs", { error });
 });
 
 // Importing the user routes.
+
 router.get("/shop", isLoggedIn, function (req, res) {
     res.render("shop");
 });
+
+
+
 
 
 module.exports = router;
